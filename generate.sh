31fd7fc95243eb7f930a518650705dcc955d541f
Andy <voxtmault@gmail.com>
2024-11-21 13:06:04 +0700

    fixed typos and added accounting proto file

--- generate.sh
+++ generate.sh
@@ -6,13 +6,13 @@ echo "Generating gRPC Codes"
 export PATH="$PATH:$(go env GOPATH)/bin"
 
 # Generate Go Codes
-protoc --go_out=./go --go-grpc_out=./go medical_record.proto patient.proto encounter.proto
+protoc --go_out=./go --go-grpc_out=./go medical_record.proto accounting.proto patient.proto encounter.proto
 echo "Go Codes Generated"
 
 # Generate JavaScript code
-protoc --js_out=import_style=commonjs,binary:./js medical_record.proto patient.proto encounter.proto
+protoc --js_out=import_style=commonjs,binary:./js medical_record.proto accounting.proto patient.proto encounter.proto
 echo "JS Code Generated"
 
 # Generate gRPC-Web code
-protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js medical_record.proto patient.proto encounter.proto
+protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js medical_record.proto accounting.proto patient.proto encounter.proto
 echo "JS Web Generated"
